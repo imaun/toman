58e630ec2a7eea6c60b9a4c609a05d899d41dc16
imaun <imun22@gmail.com>
2023-04-15 00:18:31 +0430

    adds xml docs for TomanExchangeSource

--- src/Toman.Core/TomanExchangeSource.cs
+++ src/Toman.Core/TomanExchangeSource.cs
@@ -13,16 +13,32 @@ public static class TomanExchangeSource
     //private static readonly IReadOnlyCollection<Currency> _currencies = CurrencySource.FindAll().ToList();
     private static ConcurrentDictionary<string, RialExchangeRate> _exchangeRates = new();
 
-    public static void SetRate(string currencyCode, decimal rate) 
-    {
+    /// <summary>
+    /// Set ExchangeRate for the given CurrencyCode. If the CurrencyCode is not one of the known
+    /// currencies of the "Toman" (defined in <see cref="CurrencySource"/>), then a <see cref="CurrencyNotFoundException"/> will raise.
+    /// </summary>
+    /// <param name="currencyCode">Currency Code (Symbol) ex: USD for United States Dollar. It must be one of the known currencies defined in <see cref="CurrencySource"/></param>
+    /// <param name="rate">The decimal Rate for the specified currency code.</param>
+    /// <exception cref="ArgumentNullException">If the currency code is null or empty.</exception>
+    /// <exception cref="CurrencyNotFoundException">If <see cref="Currency"/> not found in the known <see cref="CurrencySource"/></exception>
+    public static void SetRate(string currencyCode, decimal rate) {
         if(currencyCode.IsNullOrEmpty())
             throw new ArgumentNullException(nameof(currencyCode));
 
         var currency = CurrencySource.FindByCode(currencyCode);
+        if (currency is null)
+            throw new CurrencyNotFoundException(currencyCode);
+
         SetRate(currency, rate);
     }
 
-
+    /// <summary>
+    /// Set ExchangeRate for the given <see cref="Currency"/>. If the Currency is not one of the known
+    /// currencies of the "Toman" (defined in <see cref="CurrencySource"/>), then a <see cref="CurrencyNotFoundException"/> will raise.
+    /// </summary>
+    /// <param name="currency">It must be one of the known currencies defined in <see cref="CurrencySource"/></param>
+    /// <param name="rate">The decimal Rate for the specified <see cref="Currency"/>.</param>
+    /// <exception cref="CurrencyNotFoundException"></exception>
     public static void SetRate(Currency currency, decimal rate) {
         if (currency is null)
             throw new CurrencyNotFoundException();
@@ -34,6 +50,42 @@ public static class TomanExchangeSource
         }
 
         _exchangeRates.TryAdd(currency.Code, exchangeRate);
+    }
+
+    /// <summary>
+    /// Returns the <see cref="RialExchangeRate"/> info for the given <paramref name="currencyCode"/>.
+    /// If the Currency is not one of the known currencies of the "Toman" (defined in <see cref="CurrencySource"/>), 
+    /// then a <see cref="CurrencyNotFoundException"/> will raise.
+    /// </summary>
+    /// <param name="currencyCode">Currency Code (Symbol) ex: USD for United States Dollar. It must be one of the known currencies defined in <see cref="CurrencySource"/></param>
+    /// <returns><see cref="RialExchangeRate"/> info for the given <paramref name="currencyCode"/> if found.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="CurrencyNotFoundException"></exception>
+    public static RialExchangeRate? GetRate(string currencyCode) 
+    {
+        if (currencyCode.IsNullOrEmpty())
+            throw new ArgumentNullException(nameof(currencyCode));
+
+        var currency = CurrencySource.FindByCode(currencyCode);
+        if (currency is null)
+            throw new CurrencyNotFoundException(currencyCode);
+
+        return GetRate(currencyCode);
+    }
+
+    /// <summary>
+    /// Returns the <see cref="RialExchangeRate"/> info for the given <see cref="Currency"/>.
+    /// </summary>
+    /// <param name="currency">It must be one of the known currencies defined in <see cref="CurrencySource"/></param>
+    /// <returns><see cref="RialExchangeRate"/> info for the given <see cref="Currency"/>, if found.</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static RialExchangeRate? GetRate(Currency currency) {
+        if (currency is null)
+            throw new ArgumentNullException(nameof(currency));
+
+        return _exchangeRates.ContainsKey(currency.Code) 
+            ? _exchangeRates[currency.Code] 
+            : null;
     }
 
     private static RialExchangeRate? FindByCurrencyCode(string currencyCode) {
